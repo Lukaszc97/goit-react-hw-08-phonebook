8c03dfcc78e284eb79ca8e10ba7e70b51ef6451f
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-10-06 19:28:20 +0200

    store

--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -1,15 +1,32 @@
-
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import contactsReducer from './SliceReducer';
 import { authReducer } from './auth/slice';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage/session';
+
+import { persistStore, persistReducer } from 'redux-persist'; 
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token']
+};
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
 });
 
-const store = configureStore({
-  reducer: rootReducer,
-  auth: authReducer
+export const store = configureStore({
+  reducer: {
+    reducer: rootReducer,
+    auth: persistReducer(authPersistConfig, authReducer)
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
 
-export default store;
+export const persistor = persistStore(store); // Zaktualizuj eksport
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux'; 
 import App from './components/App'; 
 import './index.css';
-import store from './Redux/Store'; 
+import {persistor,store} from './Redux/Store'; 
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
       <App />
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
