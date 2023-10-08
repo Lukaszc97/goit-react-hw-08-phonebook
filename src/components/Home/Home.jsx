bc33b91c646c00f9aac583eacc901bebfe47ee1f
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-10-08 22:31:23 +0200

    Fix Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectorIsLoggedIn } from '../../Redux/auth/selectors';
 
 const Home = () => {
+
+  const isLoggedIn = useSelector(selectorIsLoggedIn);
+
   return (
     <div>
       <h2>Welcome to the Phonebook App!</h2>
       <p>This is your personal phonebook application.</p>
-      <p>Please log in or register to access your contacts.</p>
+
+      {!isLoggedIn && <p>Please log in or register to access your contacts.</p>}
     </div>
   );
 };
